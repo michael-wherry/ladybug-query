3cf530c6a9459f32a20caea6cfd68971a3b56ee4
blackninjamaw <blackninjamaw1@gmail.com>
2022-11-29 16:37:03 -0600

    Created R project and added initial script to import the data files

---
+++ InsectQuery.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(readxl)
+
+df_scanned_ladybug <- read.csv("Data/ScanLadybugData.csv")
+df_ladybug_species <- read_xlsx("Data/Ladybug Data.xlsx", .name_repair = "universal")
+df_scanned_wings <- read_xlsx("Data/Cleaned Data LWA .xlsx", .name_repair = "universal")
+df_scanned_butterfly <- read_xlsx("Data/CompletePierisData_2022-03-09.xlsx", .name_repair = "universal")
+
+df_scanned_butterfly <- rename_with(df_scanned_butterfly, ~ sub("dwc.(.)", "\\U\\1", .x, perl = TRUE))
+df_scanned_wings <- df_scanned_wings %>% drop_na() 

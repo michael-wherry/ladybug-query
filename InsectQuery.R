485e3e2e1b9d4374c5e4b2a13ff45931a9905d2d
blackninjamaw <blackninjamaw1@gmail.com>
2022-11-29 18:33:18 -0600

    Testing commit

--- InsectQuery.R
+++ InsectQuery.R
@@ -8,3 +8,5 @@ df_scanned_butterfly <- read_xlsx("Data/CompletePierisData_2022-03-09.xlsx", .na
 
 df_scanned_butterfly <- rename_with(df_scanned_butterfly, ~ sub("dwc.(.)", "\\U\\1", .x, perl = TRUE))
 df_scanned_wings <- df_scanned_wings %>% drop_na() 
+
+#addition

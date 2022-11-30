9f6df8f6e8caf5c79ffd2d2331e7b7c486bba462
blackninjamaw <blackninjamaw1@gmail.com>
2022-11-30 12:05:13 -0600

    Finsished renaming columns to camel case for each DataFrame

--- InsectQuery.R
+++ InsectQuery.R
@@ -2,10 +2,40 @@ library(tidyverse)
 library(readxl)
 
 df_scanned_ladybug <- read.csv("Data/ScanLadybugData.csv")
-df_ladybug_species <- read_xlsx("Data/Ladybug Data.xlsx", .name_repair = "universal")
+df_scanned_ladybug_species <- read_xlsx("Data/Ladybug Data.xlsx", .name_repair = "universal")
 df_scanned_wings <- read_xlsx("Data/Cleaned Data LWA .xlsx", .name_repair = "universal")
 df_scanned_butterfly <- read_xlsx("Data/CompletePierisData_2022-03-09.xlsx", .name_repair = "universal")
 
-df_scanned_butterfly <- rename_with(df_scanned_butterfly, ~ sub("dwc.(.)", "\\U\\1", .x, perl = TRUE))
-df_scanned_wings <- df_scanned_wings %>% drop_na() 
+#renaming all columns to camelCase for consistency
 
+df_butterfly <- df_scanned_butterfly %>%
+  rename_with(~ sub("(?:dwc.:?)?(.)", "\\L\\1", .x, perl = TRUE)) %>%
+  rename(coreId = coreid) %>%
+  rename(lengthLW = lWingLength) %>%
+  rename(widthLW = lWingWidth) %>%
+  rename(apexLW = lBlackPatchApex) %>%
+  rename(anteriorSpotLW = lAnteriorSpotM3) %>%
+  rename(posteriorSpotLW = lPosteriorSpotCu2) %>%
+  rename(interspotLW = lInterspotPA) %>%
+  rename(lengthRW = rWingLength) %>%
+  rename(widthRW = rWingWidth) %>%
+  rename(apexRW = rBlackPatchApex) %>%
+  rename(anteriorSpotRW = rAnteriorSpotM3) %>%
+  rename(posteriorSpotRW = rPosteriorSpotCu2) %>%
+  rename(interspotRW = rInterspotPA)
+  
+
+df_wings <- df_scanned_wings %>%
+  rename(coreId = core.ID) %>%
+  rename(lengthLW = LW.length) %>%
+  rename(widthLW = LW.width) %>%
+  rename(apexLW = LW.apex.A) %>%
+  rename(lengthRW = RW.length) %>%
+  rename(widthRW = RW.width) %>%
+  rename(apexRW = RW.apex.A) %>%
+  drop_na() #shouldn't have sparse entries
+
+df_ladybug <- df_scanned_ladybug
+
+df_ladybug_species <- df_scanned_ladybug_species %>%
+  rename(catalogNumber = SCAN.CODE)
